73e54e02e9bf121de6ccd880f5aa49c6769e5542
Gopikarthicksky <gopikarthick.r@sky.uk>
2024-05-27 16:10:30 +0530

    Updated Code

--- ryd/rides/urls.py
+++ ryd/rides/urls.py
@@ -1,11 +1,10 @@
 # urls.py
 from django.urls import path
-from .views import RideListView, RideDetailView, AvailableRidesView, FromLocationAutocompleteView, ToLocationAutocompleteView
+from .views import RideListView, RideDetailView, AvailableRidesView, LocationAutocompleteView
 
 urlpatterns = [
     path('rides/', RideListView.as_view(), name='ride_list'),
     path('rides/<int:pk>/', RideDetailView.as_view(), name='ride_detail'),
     path('available_rides/', AvailableRidesView.as_view(), name='available_rides'),
-    path('from_location_autocomplete/', FromLocationAutocompleteView.as_view(), name='from_location_autocomplete'),
-    path('to_location_autocomplete/', ToLocationAutocompleteView.as_view(), name='to_location_autocomplete'),
+    path('location_autocomplete/', LocationAutocompleteView.as_view(), name='to_location_autocomplete'),
 ]
